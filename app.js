d571d6c73f3f347ea2ed1baf67258dca90e369b0
NikolaZh <djozefs@gmail.com>
2017-11-01 18:30:46 +0300

    add server(on express) + pug

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use("/static", express.static("public"));
+
+app.get('/', function (req, res) {
+    res.render('index', {title: "TestIndex"})
+})
+
+app.use((req, res)=>{
+    res.status(404);
+    res.send("Not found 404");
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log("Server Starts on 3000 port")
+});
--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/",(req,res,next)=>{
-      res.status(200);
-      res.send(index.html)
-});
-
-
-
-
-
-app.listen(3000, () => {
-console.log("Server start on 3000 port")});
+require('babel-core/register');
+require('babel-polyfill');
+require('./app');
