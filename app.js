28f2ce83a1617de0503c58660ce79744e5af7279
NikolaZh <djozefs@gmail.com>
2017-11-13 15:07:34 +0300

    without buffer

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 app.use('/static', express.static('public'));
 app.get('/', (req, res) => {
-  res.render('index', { title: 'TestIndex' });
+    res.render('index', { title: 'TestIndex' });
 });
 app.use((req, res) => {
-  res.status(404);
-  res.send('Not found 404');
+    res.status(404);
+    res.send('Not found 404');
 });
 app.listen(3000, () => {
-  console.log('Server Starts on 3000 port');
+    console.log('Server Starts on 3000 port');
 });
--- public/js/lib/model/local-storage.js
+++ public/js/lib/model/local-storage.js
@@ -1,62 +1,63 @@
 class LocalStorage extends BaseStorage {
-  all(cls) {
-    super.all(cls);
-    const storageName = cls.prototype.constructor.name;
-    const wrapObj = localStorage.getItem(storageName);
-    if (wrapObj === null) { // check our object in storage browser
-      return false;
-    }
-    const unwrapObj = JSON.parse(wrapObj).map(fields => cls.prototype.constructor.unwrap(fields));
-    return unwrapObj;
-  }
-
-  save(object) {
-    super.save(object);
-    const storageName = object.constructor.name;
-    let data = localStorage.getItem(storageName);
-    const newAppropId = this.increase_id(object);
-    if (data) {
-      data = JSON.parse(data).map(fields => object.constructor.unwrap(fields));
-    }
-    if (data && newAppropId) { // storage and newID exist => just add new object
-      object._id = newAppropId;
-      data.push(object);
-    } else if (data) { // storage exist, but newID false (object is old) = looking for obj with same id in storage and replace
-      for (const key in data) {
-        if (data[key]._id == object._id) {
-          for (const key1 in data[key]._f) {
-            data[key]._f[key1] = object._f[key1];
-          }
+    all(cls) {
+        super.all(cls);
+        const storageName = cls.prototype.constructor.name;
+        const wrapObj = localStorage.getItem(storageName);
+        if (wrapObj === null) { // check our object in storage browser
+            return false;
         }
-      }
-    } else { // no storage, just add object
-      object._id = newAppropId;
-      data = [object];
+        const unwrapObj = JSON.parse(wrapObj).map(fields => cls.unwrap(fields));
+        return unwrapObj;
     }
-    localStorage.setItem(storageName, JSON.stringify(data));
-    return object._id;
-  }
 
-  load(cls, id) {
-    super.load(cls, id);
-    const data = this.all(cls);
-    if (data) {
-      for (const key in data) {
-        if (data[key]._id == id) { return data[key]; }
-      }
-    } else return false;
-  }
-
-  increase_id(object) {
-    const objType = object.constructor.name;
-    if (object._id != null) return false; // id exist = object old
-    if (localStorage.getItem(`${objType}_ids`) === null) {
-      localStorage.setItem(`${objType}_ids`, JSON.stringify(1));
-      return 1;
+    save(object) {
+        super.save(object);
+        let isNewObj = false;
+        const storageName = object.constructor.name;
+        let data = localStorage.getItem(storageName);
+        if (data) {
+            data = JSON.parse(data).map(fields => object.constructor.unwrap(fields));
+        }
+        if (object._id === null) {
+            object._id = this.increaseId(object);
+            isNewObj = true;
+        } // check object old or new (if new add id)
+        if (data && isNewObj) { // storage and newID exist => just add new object
+            data.push(object);
+        } else if (data) { // storage exist, but object is old = looking for obj with same id in storage and replace
+            for (const key in data) {
+                if (data[key]._id == object._id) {
+                    for (const key1 in data[key]._f) {
+                        data[key]._f[key1] = object._f[key1];
+                    }
+                }
+            }
+        } else { // no storage, just add object
+            data = [object];
+        }
+        localStorage.setItem(storageName, JSON.stringify(data));
+        return object._id;
     }
-    let lastId = JSON.parse(localStorage.getItem(`${objType}_ids`));
-    lastId++;
-    localStorage.setItem(`${objType}_ids`, JSON.stringify(lastId));
-    return lastId;
-  }
+
+    load(cls, id) {
+        super.load(cls, id);
+        const data = this.all(cls);
+        if (data) {
+            for (const key in data) {
+                if (data[key]._id == id) { return data[key]; }
+            }
+        } else return false;
+    }
+
+    increaseId(object) {
+        const objType = object.constructor.name;
+        if (localStorage.getItem(`${objType}_ids`) === null) {
+            localStorage.setItem(`${objType}_ids`, 1);
+            return 1;
+        }
+        let lastId = parseInt(localStorage.getItem(`${objType}_ids`));
+        lastId++;
+        localStorage.setItem(`${objType}_ids`, lastId);
+        return lastId;
+    }
 }
